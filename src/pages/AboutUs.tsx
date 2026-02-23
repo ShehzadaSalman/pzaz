42820f3c4b3ab9b4186ac8ff7abf825a07c54777
shahzada salman <shehzada.salman072@gmail.com>
2026-02-23 16:17:38 +0500

    fixed the about us page

--- src/pages/AboutUs.tsx
+++ src/pages/AboutUs.tsx
@@ -187,8 +187,8 @@ const AboutUs = () => {
           <div className="absolute inset-0 bg-gradient-to-r from-background/95 via-background/70 to-transparent" />
         </div>
 
-        <div className="max-w-7xl mx-auto px-6 md:px-12 relative z-10">
-          <div className="max-w-2xl">
+        <div className=" px-6 md:px-12 relative z-10 w-full">
+          <div className="max-w-6xl mx-auto">
             <motion.img
               src={aboutLogo}
               alt="Pzaz icon"
